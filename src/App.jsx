5508b4a737ab93d5f525841b62473baaeeb33db0
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-03-09 21:58:12 +0200

    add some elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import Pizza from "./Pizza";
+
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div>
+      <h1>Padre Gino's Pizza – Order Now</h1>
+      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
+      <Pizza
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+      />
+    </div>
   );
 }
 
