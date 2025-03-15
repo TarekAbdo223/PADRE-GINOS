624da6bdfebe7a91445b7807b337e1f5e18c6c26
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-03-15 16:40:53 +0200

    custom hook pizzaoftheday

---
+++ src/usePizzaOfTheDay.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetchPizzaOfTheDay("/api/pizza-of-the-day");
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
+
+export default usePizzaOfTheDay;
