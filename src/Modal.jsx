7f6c1ad66b893948f8ea115b081809c360e0940e
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-04-11 14:30:58 +0200

    end the portal

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const Modal = ({ children }) => {
   const elRef = useRef(null);
 
   if (!elRef.current) {
     elRef.current = document.createElement("div");
-    document.body.appendChild(elRef.current);
   }
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+    return "TODO";
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
 };
 
 export default Modal;
